ce50a749e3124910118471fe80c92f12d037a160
Serendisand <leng@ipacel.cc>
2026-08-30 14:44:52 +0800

    重新设计ASCII艺术字

--- Lengbanlist - main/src/main/java/org/leng/Lengbanlist.java
+++ Lengbanlist - main/src/main/java/org/leng/Lengbanlist.java
@@ -207,16 +207,16 @@ public void onEnable() {
     }
     registerFeatureCommands();
 
-    getServer().getConsoleSender().sendMessage("§b  _                      ____              _      _     _   ");
-    getServer().getConsoleSender().sendMessage("§6 | |                    |  _ \\            | |    (_)   | |  ");
-    getServer().getConsoleSender().sendMessage("§b | |     ___ _ __   __ _| |_) | __ _ __ | |     _ ___| |_ ");
-    getServer().getConsoleSender().sendMessage("§f | |    / _ \\ '_ \\ / _` |  _ < / _` | '_ \\| |    | / __| __|");
-    getServer().getConsoleSender().sendMessage("§b | |___|  __/ | | | (_| | |_) | (_| | | | | |____| \\__ \\ |_ ");
-    getServer().getConsoleSender().sendMessage("§6 |______\\___|_| |_|\\__,_|___/ \\__,_|_| |_|______|_|___/\\__|");
+    getServer().getConsoleSender().sendMessage("§b  _                            _                     _   _     _   ");
+    getServer().getConsoleSender().sendMessage("§6 | |                          | |                   | | | |   | |  ");
+    getServer().getConsoleSender().sendMessage("§b | |     ___  _ __    __ _   | |__    __ _   ___   | |_| |_  | |_ ");
+    getServer().getConsoleSender().sendMessage("§f | |    / _ \\| '_ \\  / _` |  | '_ \\  / _` | / _ \\  | __| __| | __|");
+    getServer().getConsoleSender().sendMessage("§b | |___|  __/| | | || (_| |  | |_) || (_| || (_) | | |_| |_  | |_ ");
+    getServer().getConsoleSender().sendMessage("§6 |______\\___||_| |_| \\__,_|  |_.__/  \\__,_| \\___/   \\__|\\__|  \\__|");
     getServer().getConsoleSender().sendMessage("§b                   __/ |                                    ");
     getServer().getConsoleSender().sendMessage("§f                   |___/                                     ");
     getServer().getConsoleSender().sendMessage("§6插件版本：v" + getPluginVersion());
-    getServer().getConsoleSender().sendMessage("§3服务端版本：" + Bukkit.getServer().getVersion());
+    getServer().getConsoleSender().sendMessage("§3服务端版本：" + Bukkit.getServer().getVers");
 
     new Metrics(Lengbanlist.this, 33262);
 
